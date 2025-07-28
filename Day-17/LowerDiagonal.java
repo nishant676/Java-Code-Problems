182273d27e25a7776dda1009d852c9230dd2dd22
Nishant Mishra <88461593+nishant676@users.noreply.github.com>
2025-07-28 14:50:25 +0000

    Good to go

---
+++ Day-17/LowerDiagonal.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class LowerDiagonal {
+    public static void main(String[] args) {
+
+        int[][] matrix = { {1,2,3}, 
+                            {4,5,6},
+                            {7,8,9} };
+     
+        for(int i = 0; i< matrix.length; i++)
+        {
+            for(int j = 0 ; j< matrix[i].length; j++)
+            {
+                if(i < j)
+                {
+                matrix[i][j]=0;   
+                }
+            }
+        }
+      System.out.println(Arrays.deepToString(matrix));
+    }
+}
